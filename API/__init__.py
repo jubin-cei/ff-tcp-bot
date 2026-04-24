8c1a63e29038526d3ebf7a6532dcb8254971d276
jubin-cei <jjubin@ceiamerica.com>
2026-04-24 20:13:50 +0530

    Use __all__ in API/__init__.py for explicit module exports

--- API/__init__.py
+++ API/__init__.py
@@ -1,2 +1,17 @@
-# This file marks the directory as a Python package
-# No imports needed - modules are accessed directly via: from API import module_name
+"""API Package - Protocol Buffer modules for Free Fire bot"""
+
+__all__ = [
+    'DEcwHisPErMsG_pb2',
+    'MajoRLoGinrEs_pb2',
+    'PorTs_pb2',
+    'MajoRLoGinrEq_pb2',
+    'sQ_pb2',
+    'Team_msg_pb2',
+    'RemoveFriend_Req_pb2',
+    'GetFriend_Res_pb2',
+    'spam_request_pb2',
+    'devxt_count_pb2',
+    'dev_generator_pb2',
+    'kyro_title_pb2',
+    'room_join_pb2',
+]
