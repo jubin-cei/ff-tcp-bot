a4a280dff02aef4fb5352674b75b06c7efeec5d4
jubin-cei <jjubin@ceiamerica.com>
2026-04-24 19:59:30 +0530

    Keep __init__.py simple to avoid circular imports

--- API/__init__.py
+++ API/__init__.py
@@ -1,14 +1,1 @@
 # API Package
-from . import DEcwHisPErMsG_pb2
-from . import MajoRLoGinrEs_pb2
-from . import PorTs_pb2
-from . import MajoRLoGinrEq_pb2
-from . import sQ_pb2
-from . import Team_msg_pb2
-from . import RemoveFriend_Req_pb2
-from . import GetFriend_Res_pb2
-from . import spam_request_pb2
-from . import devxt_count_pb2
-from . import dev_generator_pb2
-from . import kyro_title_pb2
-from . import room_join_pb2
